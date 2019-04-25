4d39a8c05775ab53a1fa8c7849137e32a9c777fd
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2019-04-24 19:50:06 -0700

    fixed styling added to readme, could not get clock to function correctly still

--- basics/Main.java
+++ basics/Main.java
@@ -5,21 +5,21 @@ public class Main {
 
     // testing pluralize
     int dogCount = 1;
-    System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
-
     int catCount = 2;
-    System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
-
     int turtleCount = 0;
+    System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+    System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
     System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
 
     // testing coin flip
     flipNHeads(4);
+    flipNHeads(7);
+    flipNHeads(2);
 
-    //testing clock
-    // while(true){
-    //   clock();
-    // }
+    // testing clock
+    while (true) {
+      clock();
+    }
   }
 
   public static String pluralize(String word, int num) {
@@ -35,13 +35,11 @@ public class Main {
   }
 
   public static String flipNHeads(int n) {
-
     int heads = 0;
     int flips = 0;
-    
+
     while (heads < n) {
       double x = Math.random();
-
       if (x >= .5) {
         System.out.println("heads");
         flips++;
@@ -51,22 +49,20 @@ public class Main {
         heads = 0;
         flips++;
       }
-
     }
-
     String output = "It took " + flips + " flips to flip " + n + " heads in a row.";
     System.out.println(output);
     return output;
   }
 
-  // public static String clock() {
-  //   LocalDateTime now = LocalDateTime.now();
-  //   int hour = now.getHour();
-  //   int minute = now.getMinute();
-  //   int second = now.getSecond();
-  //   String time = hour + ":" + minute + ":" + second;
-  //   System.out.println(time);
-  //   return time;
-  // }
+  public static String clock() {
 
+    LocalDateTime now = LocalDateTime.now();
+    int hour = now.getHour();
+    int minute = now.getMinute();
+    int second = now.getSecond();
+    String time = hour + ":" + minute + ":" + second;
+    System.out.println(time);
+    return time;
+  }
 }
---
+++ basics/README.md
@@ -0,0 +1,10 @@
+## Basics
+
+### Pluralize
+this method adds an 's' to a string if the number given in the method call is 0 or 2+. meanwhile the word given will remain in the singular if the number given was a 1.
+
+### Flipping Coins
+This method keeps track of the number of times 'heads' is flipped. When heads has been flipped n number of times, the methods prints a string telling the user that it took x number of times for heads to be flipped n times.
+
+### Clock
+This method utlized the machines build in clock to determine the current time. When run correctly; the mathod will print out the cruuent time once a second when the time changes. This will run non-stop until the user tells the machine to stop.
